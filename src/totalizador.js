00b43eae8c1dac633183950d1c0c24b7a815ae51
Mateo Gandarillas <mateoganav@gmail.com>
2025-03-01 17:29:00 -0400

    feat: Calcular precio total con el valor de impuesto de CA

--- src/totalizador.js
+++ src/totalizador.js
@@ -4,12 +4,12 @@ export function calcularPrecioNeto(cantidad, precio) {
 
 export function obtenerImpuestoEstado(codigo) {
     const impuestos = new Map([
-        ["UT", 6.65],
-        ["NV", 8.00],
-        ["TX", 6.25],
-        ["AL", 4.00],
         ["CA", 8.25]
     ]);
 
     return impuestos.get(codigo);
+}
+
+export function calcularPrecioImpuesto(precioNeto, impuestos) {
+    return precioNeto + (precioNeto * impuestos / 100);
 }
--- src/totalizador.test.js
+++ src/totalizador.test.js
@@ -1,7 +1,13 @@
-import { calcularPrecioNeto } from './totalizador'
+import { calcularPrecioNeto,obtenerImpuestoEstado,calcularPrecioImpuesto } from './totalizador'
 
 describe("Calcular precio neto", () => {
   it("Deberia multiplicar cantidad por precio", () => {
     expect(calcularPrecioNeto(12, 5)).toEqual(60);
+  });
+});
+
+describe("Obtener precio total con el impuesto de CA", () => {
+  it("Deberia retornar la suma del precio neto y el impuesto total de CA", () => {
+    expect(calcularPrecioImpuesto(500,obtenerImpuestoEstado("CA"))).toBeCloseTo(541.25);
   });
 });
