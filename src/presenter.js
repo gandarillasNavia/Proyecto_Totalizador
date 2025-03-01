1dc30576d61fe66c59cdbbb713a1b3277dc7eba5
unknown <huascar.rivero@ucb.edu.bo>
2025-03-01 15:48:28 -0400

    feat(ui): html para ingresar precio y mostrar

--- src/presenter.js
+++ src/presenter.js
@@ -1,8 +1,9 @@
 const form = document.getElementById("parametros")
 const inputCant = document.getElementById("cantidad-in")
-const pRes = document.getElementById("resultado")
+const inputPrecio = document.getElementById("precio-in")
+const pRespuesta = document.getElementById("resultado")
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  pRes.textContent = inputCant.value;
+  pRespuesta.textContent = inputCant.value + " * " + inputPrecio.value;
 });
