bc837f2ccd393330d4a81818c3fa3eff790edc49
unknown <huascar.rivero@ucb.edu.bo>
2025-03-01 16:38:33 -0400

    feat(ui): muestra el porcentaje de impuesto segun el codigo de estado

--- src/presenter.js
+++ src/presenter.js
@@ -1,18 +1,22 @@
-import { calcularPrecioNeto } from "./totalizador"
+import { calcularPrecioNeto } from "./totalizador";
+import { obtenerImpuestoEstado } from "./totalizador";
 
-const form = document.getElementById("parametros")
-const inputCant = document.getElementById("cantidad-in")
-const inputPrecio = document.getElementById("precio-in")
-const selectEstado = document.getElementById("estado-sel")
-const spanOperacion = document.getElementById("operacion-textual")
-const spanRespuesta = document.getElementById("resultado")
-const spanEstado = document.getElementById("estado")
+const form = document.getElementById("parametros");
+const inputCant = document.getElementById("cantidad-in");
+const inputPrecio = document.getElementById("precio-in");
+const selectEstado = document.getElementById("estado-sel");
+const spanOperacion = document.getElementById("operacion-textual");
+const spanPrecioNeto = document.getElementById("precio-neto");
+const spanCodigoEstado = document.getElementById("estado");
+const spanPorcentajeImpuesto = document.getElementById("porcentaje-impuesto");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  let cantidad = inputCant.value;
-  let precio = inputPrecio.value;
-  spanOperacion.textContent = "(" + cantidad + "* $" + precio + "):";
-  spanRespuesta.textContent = calcularPrecioNeto(parseInt(cantidad), parseInt(precio)) + "$";
-  spanEstado.textContent = selectEstado.value;
+  let cantidad = parseInt(inputCant.value);
+  let precio = parseInt(inputPrecio.value);
+  let codigo = selectEstado.value;
+  spanOperacion.textContent = `(${cantidad} * $${precio}):`;
+  spanPrecioNeto.textContent = calcularPrecioNeto(cantidad, precio) + "$";
+  spanCodigoEstado.textContent = codigo + ":";
+  spanPorcentajeImpuesto.textContent = obtenerImpuestoEstado(codigo) + "%";
 });
--- src/totalizador.js
+++ src/totalizador.js
@@ -1,3 +1,15 @@
 export function calcularPrecioNeto(cantidad, precio) {
     return cantidad * precio;
 }
+
+export function obtenerImpuestoEstado(codigo) {
+    const impuestos = new Map([
+        ["UT", 6.65],
+        ["NV", 8.00],
+        ["TX", 6.25],
+        ["AL", 4.00],
+        ["CA", 8.25]
+    ]);
+
+    return impuestos.get(codigo);
+}
