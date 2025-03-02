705e91ca3df3161b4e56f6a45c84dc194e1f9277
Mateo Gandarillas <mateoganav@gmail.com>
2025-03-01 20:50:15 -0400

    feat(ui): eleccionar la categoría de producto desde una lista, por defecto, Varios

--- src/presenter.js
+++ src/presenter.js
@@ -9,17 +9,21 @@ const selectEstado = document.getElementById("estado-sel");
 const spanOperacion = document.getElementById("operacion-textual");
 const spanPrecioNeto = document.getElementById("precio-neto");
 const spanCodigoEstado = document.getElementById("estado");
+const selectCategoria = document.getElementById("categoria-sel")
+const spanCategoria = document.getElementById("categoria");
 const spanPorcentajeImpuesto = document.getElementById("porcentaje-impuesto");
 const spanTotalImpuesto = document.getElementById("total-impuesto");
 const spanPrecioTotal = document.getElementById("precio-total");
 const spanPorcentajeDescuentoPrecio = document.getElementById("porcentaje-descuento-precio")
 const spanTotalDescuentoPrecio = document.getElementById("descuento-precio-total")
 
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   let cantidad = parseInt(inputCant.value);
   let precio = parseInt(inputPrecio.value);
   let codigo = selectEstado.value;
+  let categoria = selectCategoria.value;
   let porcentajeImpuesto = obtenerImpuestoEstado(codigo);
   let precioNeto = calcularPrecioNeto(cantidad, precio);
   let impuestoTotal = calcularImpuesto(precioNeto, porcentajeImpuesto);
@@ -29,6 +33,7 @@ form.addEventListener("submit", (event) => {
   spanOperacion.textContent = `(${cantidad} * $${precio}):`;
   spanPrecioNeto.textContent = precioNeto + "$";
   spanCodigoEstado.textContent =  codigo;
+  spanCategoria.textContent = categoria;
   spanPorcentajeImpuesto.textContent = " ( " + porcentajeImpuesto + "%) :";
   spanTotalImpuesto.textContent = impuestoTotal.toFixed(4) + "$";
   spanPrecioTotal.textContent = precioTotal + "$";
